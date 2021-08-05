b0ba8199762a9e24803e2b42115615a0dbb84637
Elena <eshipkova@iMac-Dmitry.local>
2021-08-05 07:46:55 +0300

    Second review

--- api_yamdb/titles/permissions.py
+++ api_yamdb/titles/permissions.py
@@ -4,10 +4,12 @@ from rest_framework import permissions
 class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-                and request.user.role == 'admin'
-                or request.user.is_superuser)
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated
+            and request.user.role == 'admin'
+            or request.user.is_superuser)
+        )
 
 
 class IsOwnerOrStaffOrReadOnly(permissions.BasePermission):
